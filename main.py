a2266bb803bca750d2e41e01c934111d3bbb4712
Ahmad Chaabane <achaaba1@stevens.edu>
2024-05-04 21:43:29 -0400

    Adding comments to all functions in the code

--- main.py
+++ main.py
@@ -19,13 +19,15 @@ news_api_key = os.environ.get("NEWS_API_KEY")
 
 
 client = openai.OpenAI()
+# Getting openAI API
 # you can add key directly if not inputed in and .env file
 
 model = "gpt-3.5-turbo-16k"
 # Specfying which AI we want to be used
 
 
-#retriving topic
+# Defining a function that contructs a URL for the API request, which includes a search
+# This goes through the process of success case (Status), then data extration of list of articles, then loops each article to extract information needed.
 def get_news(topic):
     url =(
     # in this part we are defining "everything" as the end point and allowing the AI model to search the web with the requirments of topics that are 5 pages long"
@@ -80,12 +82,15 @@ def get_news(topic):
     except requests.exceptions.RequestException as e: #handing error
         print("Error occured during API request",e)
 
-
-#allowing the AI to summarize the article in points
+# Here we put all the functions into a class the API interactions to maintian state and have steady info such as the assistant and threads.
+# Allowing the AI to summarize the article in points
 class AssistantManager:
+    #To avoid making multiple assistants we hard coded the ID's that were made by the below functions once it was ran once.
     thread_id = "thread_IMbHUHR3olmM5AbxGE6UsH6J"
     assistant_id = "asst_LjZUbZzoLZ2wNBFsL92z2THW"
 
+    #This function allows all the data to be stored including client,model, assistant, thread, run and summary. This is stored in here so it keeps this info saved within the class.
+    # This also makes sure that there is an existant Assistant and thread in our code before making a new one to stop repetitivness.
     def __init__(self, model: str = model)-> None:
         self.client = client
         self.model = model
@@ -123,7 +128,7 @@ class AssistantManager:
             AssistantManager.thread_id = thread_obj.id
             self.thread = thread_obj
             print(f"ThreadID::: {self.thread.id}")
-    # Add messages to thread
+    # This function is sending user inputs to the thread
     def add_messages_to_thread(self, role, content):
         if self.thread:
             self.client.beta.threads.messages.create(
@@ -131,7 +136,7 @@ class AssistantManager:
                 role = role,
                 content = content
             )
-    #passing instrcutions 
+    # Excutes the assistant with the given intructions, allowing the assistant to start operations.  
     def run_assistant(self, instructions):
         if self.thread and self.assistant:
             self.run = self.client.beta.threads.runs.create(
@@ -139,6 +144,7 @@ class AssistantManager:
                 assistant_id= self.assistant.id,
                 instructions= instructions
             )
+    # This function allows for the most recent thread to be reciewed and processed, this is then stored for it to be called later.
     def process_message(self):
         if self.thread:
             messages = self.client.beta.threads.messages.list(
@@ -153,12 +159,14 @@ class AssistantManager:
 
             self.summary = "\n".join(summary)
             print(f"SUMMARY------> {role.capitalize()}: ==> {response}")
-    #this is where everything is happened the funtion is being pulled
+    # this is where everything is happened the funtion is being pulled
     def call_required_functions(self, required_actions):
+        # Seeing if the run is active if not it will break. 
         if not self.run:
             return 
+        # initialized to collect the outputs from the various tool calls. This list will eventually be submitted back to the assistant to integrate these outputs into the ongoing run.
         tools_outputs = []
-
+        # For each action the method get the function name and its arguments. SO in this case it looks at "get_news", which we know exists so its going to run regardless but now giving it all the arguments needed.
         for action in required_actions["tool_calls"]:
             func_name = action["function"]["name"]
             arguments = json.loads(action["function"]["arguments"])
@@ -181,7 +189,9 @@ class AssistantManager:
         )
     #for streamlit
     def get_summary(self):
+        # Provides a method to access the summary created 
         return self.summary
+    # Contuinously checks the status of the assistant's run and acts accordingly, either waiting for completion or responding to additional action.
     def wait_for_completion(self):
         if self.thread and self.run:
             while True:
@@ -200,7 +210,7 @@ class AssistantManager:
                     self.call_required_functions(
                         required_actions=run_status.required_action.submit_tool_outputs.model_dump()
                     )
-    #run the steps
+    #run the steps, shows the back end process
     def run_steps(self):
         run_steps = self.client.beta.threads.runs.steps.list(
             thread_id= self.thread.id,
@@ -208,7 +218,7 @@ class AssistantManager:
         )
         print(f"RUN-Steps::: {run_steps}")
         return run_steps.data
-
+#Calls all the functions with required parameters and uses streamlit to create a platform to use them.
 def main():
     # news = get_news("bitcoin")
     # print(news[0])
